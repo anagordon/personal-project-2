f638d18b80a7919912c887aa4cff6ab163c3f0ac
anagordon <143830954+anagordon@users.noreply.github.com>
2024-04-01 00:13:10 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+	<title>FIrst php file (hopefully)</title>
+</head>
+<body>
+
+<h1>My First Local PHP File</h1>
+
+
+<<?php
+
+echo '<p>PHP is working</p>';
+
+
+ ?>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
